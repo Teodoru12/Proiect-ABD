e08d0d3176f6c294bdf15807a0ae542cbd4ad32e
Teodoru12 <david.teodor.8a@gmail.com>
2018-01-10 22:18:16 +0200

    CommitTeodor

--- projectbaze/admin.Designer.cs
+++ projectbaze/admin.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.raport_button = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // raport_button
@@ -50,12 +52,35 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "ModificarePret";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Raport-comenzi(excel)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(108, 168);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Raport-comenzi(csv)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(717, 261);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.raport_button);
             this.Name = "admin";
@@ -68,5 +93,7 @@
 
         private System.Windows.Forms.Button raport_button;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
     }
 }
--- projectbaze/admin.cs
+++ projectbaze/admin.cs
@@ -35,18 +35,19 @@ namespace projectbaze
                 pdfDoc.Close();
 
                 byte[] bytes = memoryStream.ToArray();
-                File.WriteAllBytes(@"D:\file.pdf", bytes);
-               
+                File.WriteAllBytes(@"D:\\file.pdf", bytes);
+
 
                 memoryStream.Close();
             }
         }
-        
+
 
         public admin()
         {
             InitializeComponent();
         }
+
         private void releaseObject(object obj)
         {
             try
@@ -64,77 +65,156 @@ namespace projectbaze
                 GC.Collect();
             }
         }
-
-    
-
-    private void button1_Click(object sender, EventArgs e)
+        private void CSVExport(string filename)
         {
-            var context = new DataClasses2DataContext();
-            
-            var result = from c in context.Useris
-                         join o in context.comenzi2s
-                         on c.ID equals o.user_id
-                         select new
-                         {
-                             c.Username,
-                             c.adresa,
-                             o.suma,
-                         };
-            string final1pdf;
-            foreach (var r in result)
+            try
             {
-                 final1pdf = r.adresa + ' ' + r.Username + ' ' + r.suma.ToString() + '\n';
-                GeneratePdf(r.Username);
-            }
-            // GeneratePdf(result.ToString());
-
-
-
-            //pt Excel
-
-            SqlConnection cnn;
-            string connectionString = null;
-            string sql = null;
-            string data = null;
-            int i = 0;
-            int j = 0;
-
-            Microsoft.Office.Interop.Excel.Application xlApp;
-            Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
-            Excel.Worksheet xlWorkSheet;
-            object misValue = System.Reflection.Missing.Value;
-
-            xlApp = new Excel.Application();
-            xlWorkBook = xlApp.Workbooks.Add(misValue);
-            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-
-            connectionString = "data source=servername;initial catalog=databasename;user id=username;password=password;";
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            sql = "SELECT * FROM Product";
-            SqlDataAdapter dscmd = new SqlDataAdapter(sql, cnn);
-            DataSet ds = new DataSet();
-            dscmd.Fill(ds);
-
-            for (i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
-            {
-                for (j = 0; j <= ds.Tables[0].Columns.Count - 1; j++)
+                var csv = new StringBuilder();
+                using (var context = new DataClasses2DataContext())
                 {
-                    data = ds.Tables[0].Rows[i].ItemArray[j].ToString();
-                    xlWorkSheet.Cells[i + 1, j + 1] = data;
+                    var query = from emp in context.Useris
+                                join o in context.comenzi3s
+                                on emp.ID equals o.user_id
+                                select new
+                                {
+                                    emp.Username,
+                                    emp.adresa,
+                                    o.suma
+                                };
+
+
+                    foreach (var row in query)
+                    {
+                        var newline = string.Format("{0},{1},{2}", row.Username.Trim(), row.adresa.Trim(), row.suma);
+                        csv.AppendLine(newline);
+                    }
+                    File.WriteAllText(filename, csv.ToString());
                 }
             }
-
-            xlWorkBook.SaveAs("csharp.net-informations.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
-            xlWorkBook.Close(true, misValue, misValue);
-            xlApp.Quit();
-
-            releaseObject(xlWorkSheet);
-            releaseObject(xlWorkBook);
-            releaseObject(xlApp);
-
-            MessageBox.Show("Excel file created , you can find the file c:\\csharp.net-informations.xls");
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
-    }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //PENTRU PDF
+            try
+            {
+            var pdf = new StringBuilder();
+                using (var context = new DataClasses2DataContext())
+                {
+                    var query = from emp in context.Useris
+                                join o in context.comenzi3s
+                                on emp.ID equals o.user_id
+                                select new
+                                {
+                                    emp.Username,
+                                    emp.adresa,
+                                    o.suma
+                                };
+
+
+                    foreach (var row in query)
+                    {
+                        var newline = string.Format("{0}  {1}  {2} ", row.Username.Trim(), row.adresa.Trim(), row.suma);
+                        newline += "\r\n";
+                        pdf.AppendLine(newline);
+                    }
+                    GeneratePdf(pdf.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            MessageBox.Show("Raport pdf creat!");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //pt Excel!!!!!
+            try
+            {
+                SqlConnection cnn;
+                string connectionString = null;
+                string sql = null;
+                string data = null;
+                int i = 0;
+                int j = 0;
+
+                Microsoft.Office.Interop.Excel.Application xlApp;
+                Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+                Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+                object misValue = System.Reflection.Missing.Value;
+
+                xlApp = new Microsoft.Office.Interop.Excel.Application();
+                xlWorkBook = xlApp.Workbooks.Add(misValue);
+                xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+                connectionString = "Server=.;Database=magazin;Trusted_Connection=true";
+                cnn = new SqlConnection(connectionString);
+                cnn.Open();
+                sql = "SELECT Useri.Username,Useri.adresa,comenzi3.suma FROM Useri INNER JOIN comenzi3 ON comenzi3.user_id=Useri.ID";
+                SqlDataAdapter dscmd = new SqlDataAdapter(sql, cnn);
+                DataSet ds = new DataSet();
+                dscmd.Fill(ds);
+
+                for (i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+                {
+                    for (j = 0; j <= ds.Tables[0].Columns.Count - 1; j++)
+                    {
+                        data = ds.Tables[0].Rows[i].ItemArray[j].ToString();
+                        xlWorkSheet.Cells[i + 1, j + 1] = data;
+                    }
+                }
+
+                xlWorkBook.SaveAs("D:\\raport-comenzi.xls", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                xlWorkBook.Close(true, misValue, misValue);
+                xlApp.Quit();
+
+                releaseObject(xlWorkSheet);
+                releaseObject(xlWorkBook);
+                releaseObject(xlApp);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            MessageBox.Show("Raport excel creat!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //PENTRU CSV
+            try
+            {
+                CSVExport(@"D:\\raport-csv");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            MessageBox.Show("Raport csv creat!");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Form ModificaPret = new Form();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
     }
 }
